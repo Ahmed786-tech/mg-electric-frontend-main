0e9c8615fa03f9dfd6f84d71582bc4d242745b96
Aamir825 <sk0939062@gmail.com>
2025-08-01 23:31:47 +0500

    fix: correct image imports in teamData.js

--- src/Components/ServicesCarousel/teamData.js
+++ src/Components/ServicesCarousel/teamData.js
@@ -1,25 +1,25 @@
-import img1 from "/public/images/IMG_0207.jpg";
-import img2 from "/public/images/IMG_2502.jpg";
-import img3 from "/public/images/IMG_0217.jpg";
+// import img1 from "/images/IMG_0207.jpg";
+// import img2 from "/images/IMG_2502.jpg";
+// import img3 from "/images/IMG_0217.jpg";
 export const teamData = [
   {
     id: 1,
     name: "Electrical Installations & Upgrades",
-    image: img1,
+    image: "/images/IMG_0207.jpg",
     description:
       "From new installations to upgrades, we provide comprehensive healthcare & commercial electrical solutions.",
   },
   {
     id: 2,
     name: "Electrical Testing & Inspections",
-    image: img2,
+    image: "/images/IMG_2502.JPG",
     description:
       "Regular testing and inspections ensure your facility's electrical systems are safe and compliant.",
   },
   {
     id: 3,
     name: "Backup power solutions and emergency lighting",
-    image: img3,
+    image: "/images/IMG_0217.jpg",
     description:
       "we offer reliable back up power solutions and emergency lighting backup power systems to keep your facility running during power outages.",
   },
